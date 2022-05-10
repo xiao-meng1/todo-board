6c82d3e527b44e3dd7b83b83ea08b257e175542c
Xiao <xmeng1@ualberta.ca>
2022-05-10 13:01:05 -0600

    add app plan to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './styles.css';
+
+
+//Planning
+//Index
+//  controller.init();
+//Model
+//  board (array) -> list (object: name (num), color (str), content (array), 
+//                      updateName(), updateColor(),
+//                      delete(), deleteAllCompletedTasks())
+//      content (array) -> task (object: name (str), dateTime (str), 
+//                              priority (str), complete (boolean), 
+//                              updateName(), updateDateTime(), updatePriority(), updateComplete(),
+//                              delete())
+//  init(), createList(), createTask(), addList(), addTask(), getList(), getTask(), updateList(),
+//      updateTask(), deleteList(), deleteTask()
+//
+//View
+//  DOMBodyModel (nodelist)
+//  newListPopup (nodeList)
+//  newTaskPopup (nodelist)
+//  editListPopup (nodeList)
+//  editTaskPopup (nodeList)
+//  init(), renderDOMBody(), addPopup(), removePopup(), createList(), 
+//  createTask(), addList(), addTask()
+//  
+//Controller
+//  init(), addNewListPopup(e), addNewTaskPopup(e), exitPopup(e), 
+//  createNewList(e), createNewTask(e), updateList(e), updateTask(e),
+//  deleteList(e), deleteAllCompletedLists(e), deleteTask(e)
